a084e24916a35427c0262ad1f98bb496215bb3e5
Soroush Mehraein <soroushmehraein@gmail.com>
2016-01-18 14:52:31 -0800

    Format files with gofmt
    Thanks to go-vim with auth gofmt on save.

--- basic_server.go
+++ basic_server.go
@@ -1,52 +1,51 @@
 package main
 
 import (
-  "io"
-  "fmt"
-  "net/http"
-  "html/template"
-  "github.com/smehraein/basic_server/utils/sanitize"
+	"fmt"
+	"github.com/smehraein/basic_server/utils/sanitize"
+	"html/template"
+	"io"
+	"net/http"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "Hello world!")
+	io.WriteString(w, "Hello world!")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
- if r.Method == "GET" {
-    t, _ := template.ParseFiles("login.gtpl")
-    t.Execute(w, nil)
- } else {
-    r.ParseForm()
-    // Display values
-    fmt.Println("username:", r.Form["username"])
-    fmt.Println("password:", r.Form["password"])
-    fmt.Println("validEmail:", sanitize.Email(r.Form["email"][0]))
-    io.WriteString(w, "Logged in!")
- }
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+		// Display values
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+		fmt.Println("validEmail:", sanitize.Email(r.Form["email"][0]))
+		io.WriteString(w, "Logged in!")
+	}
 }
 
 var mux map[string]func(http.ResponseWriter, *http.Request)
 
 func main() {
-  server := http.Server{
-    Addr:   ":8000",
-    Handler: &myHandler{},
-  }
-  mux = make(map[string]func(http.ResponseWriter, *http.Request))
-  mux["/login"] = login
-  mux["/"] = hello
-  server.ListenAndServe()
+	server := http.Server{
+		Addr:    ":8000",
+		Handler: &myHandler{},
+	}
+	mux = make(map[string]func(http.ResponseWriter, *http.Request))
+	mux["/login"] = login
+	mux["/"] = hello
+	server.ListenAndServe()
 }
 
 type myHandler struct{}
 
 func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  if h, ok := mux[r.URL.String()]; ok {
-    h(w, r)
-    return
-  }
+	if h, ok := mux[r.URL.String()]; ok {
+		h(w, r)
+		return
+	}
 
-  io.WriteString(w, "My server: "+r.URL.String())
+	io.WriteString(w, "My server: "+r.URL.String())
 }
-
--- utils/sanitize/select_test.go
+++ utils/sanitize/select_test.go
@@ -3,19 +3,19 @@ package sanitize
 import "testing"
 
 func TestSelect(t *testing.T) {
-  cases := []struct {
-    in string
-    choices []string
-    want bool
-  }{
-    {"apple", []string{"apple", "banana", "orange"}, true},
-    {"banana", []string{"apple", "banana", "orange"}, true},
-    {"egg", []string{"apple", "banana", "orange"}, false},
-  }
-  for _, c := range cases {
-    got := Select(c.in, c.choices)
-    if got != c.want {
-      t.Errorf("Select(%q) with choices(%q) == %t, want %t", c.in, c.choices, got, c.want)
-    }
-  }
+	cases := []struct {
+		in      string
+		choices []string
+		want    bool
+	}{
+		{"apple", []string{"apple", "banana", "orange"}, true},
+		{"banana", []string{"apple", "banana", "orange"}, true},
+		{"egg", []string{"apple", "banana", "orange"}, false},
+	}
+	for _, c := range cases {
+		got := Select(c.in, c.choices)
+		if got != c.want {
+			t.Errorf("Select(%q) with choices(%q) == %t, want %t", c.in, c.choices, got, c.want)
+		}
+	}
 }

7f72f1328fcecc834de09d2dbd11b23b8f2ddbee
Soroush Mehraein <soroushmehraein@gmail.com>
2016-01-19 01:12:31 -0800

    Implement basic PostgreSQL connectivity + rename
    Server now exits if unable to connect to the database

--- basic_server.go
+++ basic_server.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/smehraein/basic_server/utils/sanitize"
+	"github.com/smehraein/pgdbGO"
+	"github.com/smehraein/sanitizeGO"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 )
 
+var db *sql.DB
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world!")
+	err := db.Ping()
+	if err != nil {
+		io.WriteString(w, "Awwww.")
+	} else {
+		io.WriteString(w, "GOT DB")
+	}
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +31,7 @@ func login(w http.ResponseWriter, r *http.Request) {
 		// Display values
 		fmt.Println("username:", r.Form["username"])
 		fmt.Println("password:", r.Form["password"])
-		fmt.Println("validEmail:", sanitize.Email(r.Form["email"][0]))
+		fmt.Println("validEmail:", sanitizeGO.Email(r.Form["email"][0]))
 		io.WriteString(w, "Logged in!")
 	}
 }
@@ -32,6 +42,12 @@ func main() {
 	server := http.Server{
 		Addr:    ":8000",
 		Handler: &myHandler{},
+	}
+	DBINFO := pgdbGO.PGConnection{"postgres", "", "postgres", "localhost"}
+	var connErr error
+	db, connErr = pgdbGO.Connect(DBINFO)
+	if connErr != nil {
+		log.Fatal(connErr)
 	}
 	mux = make(map[string]func(http.ResponseWriter, *http.Request))
 	mux["/login"] = login
